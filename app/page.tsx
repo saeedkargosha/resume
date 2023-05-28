195273041721984ce3981a2aa345c2c3c8906a1c
saeed kargosha <saeed.k@cosmopolis.co>
2023-05-28 19:33:38 +0330

    fix active nav

--- app/page.tsx
+++ app/page.tsx
@@ -5,21 +5,18 @@ export default function Home() {
         {
           'I am a self-taught JavaScript developer with over 7 years of programming experience. I have worked on multiple international projects of various scales in mobile, web apps, and backend development.'
         }
-        <p>
-          {
-            'My diverse background enables me to contribute across the entire stack, but my greatest strengths lie in front-end development.'
-          }
-        </p>
-        <p>
-          {
-            'I have good knowledge of UI design, which allows me to bridge the gap between design and development. As a developer, I know how to deliver productive and maintainable software and take ownership of the quality of my work.'
-          }
-        </p>
-        <p>
-          {
-            'I am passionate about my work and constantly strive to improve. I am always open to constructive criticism and feedback, as I believe they are essential parts of personal and professional growth'
-          }
-        </p>
+        <br />
+        {
+          'My diverse background enables me to contribute across the entire stack, but my greatest strengths lie in front-end development.'
+        }
+        <br />
+        {
+          'I have good knowledge of UI design, which allows me to bridge the gap between design and development. As a developer, I know how to deliver productive and maintainable software and take ownership of the quality of my work.'
+        }
+        <br />
+        {
+          'I am passionate about my work and constantly strive to improve. I am always open to constructive criticism and feedback, as I believe they are essential parts of personal and professional growth'
+        }
       </p>
     </div>
   )
--- components/sidebar/nav-item.tsx
+++ components/sidebar/nav-item.tsx
@@ -16,12 +16,12 @@ export function NavItem({
         'h-20 flex flex-row gap-5 w-full items-center pl-11 rounded-s-full mb-1 text-neutral-500 relative',
         {
           'bg-neutral-100': isActive,
-          'text-accent-600': isActive,
+          '!text-accent-600': isActive,
         }
       )}>
       <div className={clsx({ 'nav-item__active-up': isActive })} />
       <span
-        className={clsx('text-neutral-400', { 'text-accent-600': isActive })}>
+        className={clsx('text-neutral-400', { '!text-accent-600': isActive })}>
         {icon}
       </span>
       <span className='text-xl'>{name}</span>
