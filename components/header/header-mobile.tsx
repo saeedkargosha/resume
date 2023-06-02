c28b056834f4229830db1353535e284b0ff09af4
saeed kargosha <saeed.k@cosmopolis.co>
2023-06-02 12:03:51 +0330

    add animation in the menu

--- components/header/header-mobile.tsx
+++ components/header/header-mobile.tsx
@@ -52,61 +52,69 @@ export default function HeaderMobile() {
               '!w-9/12 !px-5': isOpen,
             }
           )}>
-          <div className='flex flex-col items-center w-fit'>
-            <div className='w-20 h-20 text-center rounded-full mb-3 overflow-hidden'>
-              <Image
-                alt={'Saeed Kargosha'}
-                src={avatar}
-                className='rounded-full'
-                placeholder='blur'
-                width={80}
-                height={80}
-                priority
-              />
+          <div
+            className={clsx(
+              'opacity-0 transition-all ease-in-out duration-1000',
+              {
+                'opacity-100': isOpen,
+              }
+            )}>
+            <div className='flex flex-col items-center w-fit'>
+              <div className='w-20 h-20 text-center rounded-full mb-3 overflow-hidden'>
+                <Image
+                  alt={'Saeed Kargosha'}
+                  src={avatar}
+                  className='rounded-full'
+                  placeholder='blur'
+                  width={80}
+                  height={80}
+                  priority
+                />
+              </div>
+              <h1 className=' text-neutral-800 text-base font-medium mb-1'>
+                {'Saeed Kargosha'}
+              </h1>
+              <span className='text-neutral-500 text-xs font-medium '>
+                {'Senior Front End Developer'}
+              </span>
             </div>
-            <h1 className=' text-neutral-800 text-base font-medium mb-1'>
-              {'Saeed Kargosha'}
-            </h1>
-            <span className='text-neutral-500 text-xs font-medium '>
-              {'Senior Front End Developer'}
-            </span>
-          </div>
-          <ul className='list-none mt-11 space-y-7'>
-            {Object.entries(navItems).map(([path, { name, icon }]) => {
-              const isActive = path === pathname
-              return (
-                <li key={path} onClick={toggle}>
-                  <Link href={path}>
-                    <NavItem name={name} isActive={isActive} icon={icon} />
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-          <div className='bg-neutral-100 h-[2px] rounded-[2px] my-6' />
-          <div className='flex gap-4 mb-8'>
-            <SocialItem social={SOCIALS.github} />
-            <SocialItem social={SOCIALS.dribble} />
-            <SocialItem social={SOCIALS.linkdin} />
-          </div>
-          <div className='space-y-4 text-neutral-400'>
-            <div className='flex items-start space-x-3'>
-              <PhoneOutlineIcon />
-              <p className='text-sm text-neutral-900 font-medium'>
-                {'+98 936 078 1422'}
-              </p>
+            <ul className='list-none mt-11 space-y-7'>
+              {Object.entries(navItems).map(([path, { name, icon }]) => {
+                const isActive = path === pathname
+                return (
+                  <li key={path} onClick={toggle}>
+                    <Link href={path}>
+                      <NavItem name={name} isActive={isActive} icon={icon} />
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <div className='bg-neutral-100 h-[2px] rounded-[2px] my-6' />
+            <div className='flex gap-4 mb-8'>
+              <SocialItem social={SOCIALS.github} />
+              <SocialItem social={SOCIALS.dribble} />
+              <SocialItem social={SOCIALS.linkdin} />
             </div>
-            <div className='flex items-start space-x-3'>
-              <EmailOutlineIcon />
-              <p className='text-sm text-neutral-900 font-medium'>
-                {'saeedkargosha@gmail.com'}
-              </p>
-            </div>
-            <div className='flex items-start space-x-3'>
-              <LocationOutlineIcon />
-              <p className='text-sm text-neutral-900 font-medium'>
-                {'Tehran - Iran'}
-              </p>
+            <div className='space-y-4 text-neutral-400'>
+              <div className='flex items-start space-x-3'>
+                <PhoneOutlineIcon />
+                <p className='text-sm text-neutral-900 font-medium'>
+                  {'+98 936 078 1422'}
+                </p>
+              </div>
+              <div className='flex items-start space-x-3'>
+                <EmailOutlineIcon />
+                <p className='text-sm text-neutral-900 font-medium'>
+                  {'saeedkargosha@gmail.com'}
+                </p>
+              </div>
+              <div className='flex items-start space-x-3'>
+                <LocationOutlineIcon />
+                <p className='text-sm text-neutral-900 font-medium'>
+                  {'Tehran - Iran'}
+                </p>
+              </div>
             </div>
           </div>
         </nav>
