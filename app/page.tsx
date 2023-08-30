878db1c0bd7bbf2a59e133ff69958edfb5dc74ee
saeed kargosha <saeedkargosha@gmail.com>
2023-08-30 14:22:26 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
     <div className='lg:py-6 lg:px-9'>
       <p className='text-neutral-600 font-medium text-sm leading-7 lg:text-xl lg:leading-[50px]'>
         {
+          'This is Saeed. During my time in high school, I started learning programming. I love what I am doing, and it makes me happy. My friends tell me that I always have a smile on my face. When I was in university, I established a computer society to help fellow students learn new things. I also enjoy contributing to open-source projects. I listen to podcasts about life and career. For instance, I enjoy listening to "Andrew Huberman," and I pay attention to both my physical and mental well-being. I also enjoy playing the piano and I am currently learning to play it. If you're interested, you can listen to "Lang Lang - Intermezzo No. 1 in E Minor," a piece that I always enjoy. I have a strong inclination towards communication, both with people in general and within my career. With our team, we work together to tackle new challenges. I'm eager to learn more and take on new opportunities.'
+        }
+        {
           'Experienced Senior Frontend Engineer skilled in web application development with a strong focus on TypeScript, React, and Next.js. With 7 years of experience, I possess a solid background in both backend and UI design, allowing me to effectively bridge the gap between design and development. My portfolio includes successful projects involving decentralized systems, e-commerce, and SaaS applications. Proficient in agile development methodologies, I have efficiently utilized tools like JIRA throughout my career. I take great pride in delivering productive and maintainable software while taking full ownership of the quality of my work.'
         }
       </p>
